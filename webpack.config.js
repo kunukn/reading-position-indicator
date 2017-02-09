5e9cb8cd2db0dbf4bf99e37f65164d725d1af1ce
Kunuk Nykjær <kunukn@gmail.com>
2017-02-09 23:51:12 +0100

    upd script build prod and minifications

--- webpack.config.js
+++ webpack.config.js
@@ -7,22 +7,67 @@ const webpack = require('webpack'),
 module.exports = (env = {}) => {
 
     // Variables set by npm scripts in package.json
-    const isProduction = env.production === true;
-    const platform = env.platform; // 'default' by default
+    const isProd = env.production === true;
+    const nodeEnv = isProd
+        ? 'production'
+        : 'development';
+
+    console.log(nodeEnv);
+
+    const plugins = [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify(nodeEnv)
+            }
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            //hash: 'true',
+            template: 'src/index.html',
+            inject: 'head'
+        }),
+        new ExtractTextPlugin({filename: '[name].bundle.css', allChunks: true})
+    ];
+
+ 
+    if (isProd) {
+        plugins.push(new webpack.LoaderOptionsPlugin({minimize: true, debug: false}));
+        plugins.push(new webpack.optimize.UglifyJsPlugin({
+            minimize: true,         
+            compress: {
+                warnings: false,
+                screw_ie8: true,
+                conditionals: true,
+                unused: true,
+                comparisons: true,
+                sequences: true,
+                dead_code: true,
+                evaluate: true,
+                if_return: true,
+                join_vars: true
+            },
+            output: {
+                comments: false
+            }
+        }));
+    } else {
+        //plugins.push(new webpack.HotModuleReplacementPlugin());
+    }
+
 
     return {
-        devtool: 'eval',
+        devtool: isProd ? 'source-map' : 'eval',
         context: path.resolve('./'),
         entry: {
             //demo: ['./src/assets/demo'],
-            rpi: ['./src/index'],            
+            rpi: ['./src/index']
         },
         output: {
             path: path.join(__dirname, 'dist'),
             publicPath: '',
             filename: '[name].bundle.js',
             chunkFilename: '[id].chunk.js',
-            libraryTarget: "umd",        
+            libraryTarget: "umd",
             library: "ReadingPositionIndicator"
         },
         devServer: {
@@ -30,66 +75,45 @@ module.exports = (env = {}) => {
             contentBase: './',
             noInfo: true,
             port: 3333,
+            compress: isProd,
+            inline: !isProd,
+            //hot: !isProd,
         },
-        plugins: [
-            new webpack.DefinePlugin({
-                PRODUCTION: JSON.stringify(isProduction),
-                PLATFORM: JSON.stringify(platform)
-            }),
-            new HtmlWebpackPlugin({
-                filename: 'index.html',
-                //hash: 'true',
-                template: 'src/index.html',
-                inject: 'head',
-            }),
-             new ExtractTextPlugin({
-                filename: '[name].bundle.css',
-                allChunks: true,
-            }),
-        ],
+        plugins: plugins,
         module: {
-            rules: [                
+            rules: [
                 {
                     test: /\.js$/,
                     loader: 'babel-loader',
-                    exclude: [/node_modules/],
-                },
-                {
+                    exclude: [/node_modules/]
+                }, {
                     test: /\.html$/,
                     loader: 'html-loader',
-                    exclude: [/node_modules/],
-                },
-                {
+                    exclude: [/node_modules/]
+                }, {
                     test: /\.css$/,
-                    loader: ExtractTextPlugin.extract({
-                        loader: 'css-loader?importLoaders=1',
-                    }),
-                    exclude: [/node_modules/],
-                },
-                {
+                    loader: ExtractTextPlugin.extract({loader: 'css-loader?importLoaders=1'}),
+                    exclude: [/node_modules/]
+                }, {
                     test: /\.(sass|scss)$/,
-                    use: [
-                        'style-loader',
-                        'css-loader',
-                        'sass-loader',
-                    ]
-                },
-                {
+                    use: ['style-loader', 'css-loader', 'sass-loader']
+                }, {
                     test: /\.(png|gif|jpg)$/,
                     loader: 'url-loader',
-                    options: { limit: '25000' },
-                    exclude: [/node_modules/],
-                },
-                {
+                    options: {
+                        limit: '25000'
+                    },
+                    exclude: [/node_modules/]
+                }, {
                     test: /\.(ttf|eot|svg)$/,
                     loader: 'file-loader',
-                    exclude: [/node_modules/],
+                    exclude: [/node_modules/]
                 }
             ]
         },
         resolve: {
-            extensions: ['.js'],
+            extensions: ['.js']
         },
-        externals: {},
+        externals: {}
     }
 };
